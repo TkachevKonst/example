019141f47aa747a14ed885d187235663b88fb255
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-11 18:11:21 +0600

    example

---
+++ example/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write ("Введите первое число :");
+int numA = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число :");
+int numB = int.Parse(Console.ReadLine());
+if (Math.Pow(numB,2)==numA)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
