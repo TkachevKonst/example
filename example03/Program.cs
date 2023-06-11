5588c5e1175f7d3ff691c5e8291be2cf7b06dbae
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-11 19:05:41 +0600

    example 03

---
+++ example03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Ведите имя :");
+string name = Console.ReadLine()!;
+if (name.ToLower() == "костя")
+{
+    Console.WriteLine($"Привет, Костян!!!");
+}
+else
+{
+    Console.WriteLine($"Привет, {name}!!!");
+}
