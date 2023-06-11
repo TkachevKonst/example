a3c5152d6eb9304229528f863c28e28763f7d86e
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-11 19:37:29 +0600

    example 04

---
+++ example04/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int xa=40, ya=10,
+    xb=1, yb=40,
+    xc=80, yc=40;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
+int x=xa, y=ya;
+int Count=0;
+while (Count < 10000)
+{
+    int n = new Random ().Next(0,3);
+    if (n==0)
+    {
+        x=(x+xa)/2;
+        y=(y+ya)/2;
+    }
+    if (n==1)
+    {
+        x=(x+xb)/2;
+        y=(y+yb)/2;
+    }
+    if (n==2)
+    {
+        x=(x+xc)/2;
+        y=(y+yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+    Count++;
+}
