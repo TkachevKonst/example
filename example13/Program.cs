44c87558c9b994f5c63f177c718ae88da339da11
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-19 11:42:36 +0600

    1

--- example13/Program.cs
+++ example13/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите координаты точки 1");
-int x1 = int.Parse(Console.ReadLine()!);
-int y1 = int.Parse(Console.ReadLine()!);
+double x1 = double.Parse(Console.ReadLine()!);
+double y1 = double.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координаты точки 2");
-int x2 = int.Parse(Console.ReadLine()!);
-int y2 = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Ответ = {Math.Sqrt(Math.Pow(x1-x2,2))+Math.Pow(y1-y2,2)}");
+double x2 = double.Parse(Console.ReadLine()!);
+double y2 = double.Parse(Console.ReadLine()!);
+double rez = Math.Sqrt(Math.Pow(x1-x2,2))+Math.Pow(y1-y2,2);
+Console.WriteLine($"Ответ = {rez:f2}");
