97bcf103c867b604389cfbdee5b75e16be71b644
Tkachev Konstantin <t_k_a90@mail.ru>
2023-06-20 14:57:46 +0600

    Добавил Задачи с семинара

---
+++ example15/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+string text1 = "Ах, простите, что я в пылу нашей ученой беседы забыл представиться. Профессор черной магии Воланд."
++ "Приглашен в Москву для консультации. Здесь в государственной библиотеке обнаружены подлинные рукописи чернокнижника Герберта Аврилакского,"
++ "десятого века. Требуется, чтобы я их разобрал. Я единственный специалист в мире. Тш-ш! Имейте в виду: Иисус — существовал.";
+Console.WriteLine(text1);
+string Replace (string text, char Oldsim, char Newsim)
+{
+string rezult = String.Empty;
+int length = text.Length;
+for (int i = 0; i < length; i++)
+{
+    if (text[i] == Oldsim)
+    {
+        rezult=rezult+Newsim;
+    }else
+    {
+        rezult=rezult+text[i];
+    }
+}    return rezult;
+}
+string newtext = Replace (text1, ' ', '_');
+Console.WriteLine(newtext);
+string newtext1 = Replace (newtext, 'п', 'П');
+Console.WriteLine(newtext1);
+string newtext2 = Replace (newtext1, 'о', 'О');
+Console.WriteLine(newtext2);
---
+++ example16/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементтов в массиве:");
+int N = int.Parse(Console.ReadLine());
+int[] arr = new int [N];
+void FullAll (int[] collec)
+{
+int length = collec.Length;
+
+for (int i = 0; i < length; i++)
+{
+     collec [i] = new Random().Next(1,100);
+}
+}
+void Print (int[] arroy)
+{
+int length = arroy.Length;
+for (int i = 0; i < length; i++)
+{
+    Console.Write($" {arroy[i]}");
+}
+}
+int IndexOff (int[] arroy1, int num)
+{
+int position = -1;
+    for (int i = 0; i < arroy1.Length; i++)
+{
+    if (arroy1[i]==num)
+    {
+      position = i; 
+      break;
+    }
+}
+
+    return position;
+}
+void Max (int[] arroy2)
+{
+int len = arroy2.Length;
+for (int i = 0; i < len-1; i++)
+{
+    int indmax =i;
+    for (int j = i+1; j < len; j++)
+    {
+        if (arroy2[j]>arroy2[indmax])
+        {
+            indmax = j;
+
+    }
+    }
+    int temp = arroy2[i];
+    arroy2[i] = arroy2[indmax];
+    arroy2[indmax] = temp;
+}
+}
+void Min (int[] arroy3)
+{int len = arroy3.Length;
+for (int i = 0; i < len-1; i++)
+{
+    int indmin =i;
+    for (int j = i+1; j < len; j++)
+    {
+        if (arroy3[j]<arroy3[indmin])
+        {
+            indmin = j;
+
+    }
+    }
+    int temp = arroy3[i];
+    arroy3[i] = arroy3[indmin];
+    arroy3[indmin] = temp;}
+}
+FullAll (arr);
+Print (arr);
+Console.WriteLine();
+int pos = IndexOff (arr, 55);
+Console.WriteLine(pos);
+Max (arr);
+Print (arr);
+Console.WriteLine();
+Min (arr);
+Print (arr);
