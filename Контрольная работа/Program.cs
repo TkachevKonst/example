d630be41731853867e3dc051ccf1ffe4894ddca5
Tkachev Konstantin <t_k_a90@mail.ru>
2023-07-26 20:12:04 +0600

    Контрольная работа

---
+++ Контрольная работа/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+Clear();
+/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.*/
+string[] array = { "hello", "friend", "end", "matrix", "35", "365", "yes" };
+int size = СounterArray(array);
+string[] newArray = NewArray(array, size);
+Write($"В массиве [{String.Join(", ", array)}],\n количество элементов длина которых меньше, либо равна 3 символам равно {size}.\n Новый массив [{String.Join(", ", newArray)}]");
+
+string[] NewArray(string[] arr, int count)
+{
+    string[] newArr = new string[count];
+    int newCount = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[newCount] = arr[i];
+            newCount++;
+        }
+    }
+    return newArr;
+}
+
+int СounterArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Write(arr[i]+ ", ");
+
+    }
+}
